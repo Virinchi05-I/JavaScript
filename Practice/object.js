22f9f9998678388718f22034f00efa6ac2603a37
Tushar Kumar Pathak <tkpathak2505@gmail.com>
2025-09-10 18:20:40 +0530

    Dom revision complete

---
+++ Practice/object.js
@@ -0,0 +1,36 @@
+/* // using the Object constructor or singleton object || Assign operator
+let obj1 = {1: "a", 2: "b"}
+let obj2 = {3: "c", 4: "d"}
+
+const obj3 = Object.assign({}, obj1, obj2)
+
+console.log(obj3);
+ */
+
+/* const object = {
+};
+
+Object.defineProperty(object, "username", {
+    value: "Tushar",
+    writable: true,
+    enumerable: true,
+    configurable: true,
+});
+
+console.log(object.username) */
+
+/* let user = {
+    username: "Tushar",
+
+    WelcomeMessage: function(){
+        console.log("welocome to the website")
+        console.log(this)
+    }
+}
+
+user.WelcomeMessage()
+ */
+
+/* const obj = Object.fromEntries([["username", "Tushar"], ["price", 999], ["isLoggedin", true]])
+console.log(obj) */
+
